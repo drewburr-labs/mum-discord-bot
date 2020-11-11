3b43d2efee8ed522ebaaf361f5737612b0f39dd8
drewburr <drewburr9@gmail.com>
2020-11-10 21:02:40 -0600

    Remove deprecated code

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ import src.self_roles as self_roles
 import src.server_stats as server_stats
 import src.admin_logging as admin_logging
 import src.server_rules as server_rules
-import src.rule_suggestions as rule_suggestions
 import io
 import asyncio
 
@@ -637,6 +636,5 @@ self_roles.setup(BOT, logger)
 server_stats.setup(BOT, logger)
 admin_logging.setup(BOT, logger)
 server_rules.setup(BOT, logger)
-rule_suggestions.setup(BOT, logger)
 
 BOT.run(TOKEN)
