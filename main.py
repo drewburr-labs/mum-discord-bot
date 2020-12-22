0d81cc9a387352baebeac8f5e1ba313d25856823
drewburr <drewburr9@gmail.com>
2020-12-22 13:00:57 -0600

    Remove members command

--- main.py
+++ main.py
@@ -90,32 +90,6 @@ async def on_ready():
     logger.info(f'{BOT.user.name} has connected to Discord!')
 
 
-@BOT.command(name="members", aliases=["member"])
-async def members(ctx):
-    """
-    Prints the number of members in the member role.
-    """
-    role_name = "Member"
-
-    role = utils.get(ctx.guild.roles, name=role_name)
-    member = ctx.author
-
-    if role in member.roles:
-        message = f'{member.mention} has agreed to the rules.'
-    else:
-        message = f'{member.mention} has not yet agreed to the rules.'
-
-    member_count = len(role.members)
-
-    embed_data = {
-        "title": f'Member Role Total',
-        "description": f'{member_count} people have agreed to the rules.\n\n{message}'
-    }
-
-    embed = discord.Embed.from_dict(embed_data)
-    await ctx.channel.send(embed=embed)
-
-
 @BOT.event
 async def on_command_error(ctx, error):
     """
