ea489c4f74bb97ed2e2ad546ef704b107c83a345
drewburr <drewburr9@gmail.com>
2021-10-09 19:08:37 -0500

    Update build command to ignore cache

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo 'Sourcing local env...'
 source .env
 
 echo 'Building Dockerfile...'
-docker build -t $NAME .
+docker build --no-cache -t $NAME .
 
 echo 'Starting container...'
 docker run --rm --name $NAME -e DISCORD_TOKEN=$DISCORD_TOKEN $NAME
