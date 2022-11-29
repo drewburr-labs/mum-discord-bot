532777bdc1e45b9cc3a9a3417af913fb67afe36a
drewburr <drewburr9@gmail.com>
2022-11-29 00:38:09 -0600

    Remove member leave logs

--- src/admin_logging.py
+++ src/admin_logging.py
@@ -14,12 +14,6 @@ class admin_logging(commands.Cog):
         self.leave_channel_name = 'leave-log'
         self.bot_log_channel_name = 'bot-logs'
 
-    @commands.Cog.listener()
-    async def on_member_remove(self, member):
-        channel = utils.get(
-            member.guild.text_channels, name=self.leave_channel_name)
-        await channel.send(f"{member.display_name} ({member.name}#{member.discriminator}) has left the server.")
-
     async def bot_log(self, guild, msg=None, embed=None):
         """
         Sends a message to the defined bot_log_channel_name.
