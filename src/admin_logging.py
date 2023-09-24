383f8069d9f96db0fef81b95800ed2451d3c8e39
drewburr <drewburr9@gmail.com>
2023-09-24 14:11:35 -0400

    Update admin logger to use controller

--- src/admin_logging.py
+++ src/admin_logging.py
@@ -8,13 +8,13 @@ from discord import utils
 from logging import Logger
 
 class admin_logging(commands.Cog):
-    def __init__(self, bot: commands.Bot, logger: Logger, guild_id: str):
+    def __init__(self, bot: commands.Bot, logger: Logger, guild_id: int, channel_id: int):
         self.bot: commands.Bot = bot
         self.logger = logger
-        self.channel_name = 'bot-logs'
         self.guild_id= guild_id
+        self.channel_id = channel_id
 
-    def get_controllerget_guild_guild(self):
+    def get_guild(self):
         return utils.get(self.bot.guilds, id=self.guild_id)
 
     async def log(self, msg=None, embed=None):
@@ -24,17 +24,22 @@ class admin_logging(commands.Cog):
         guild = self.get_guild()
 
         channel = utils.get(guild.text_channels,
-                            name=self.channel_name)
+                            id=self.channel_id)
 
-        if msg:
-            await channel.send(msg)
+        try:
+            if msg:
+                await channel.send(msg)
 
-        if embed:
-            await channel.send(embed=embed)
+            if embed:
+                await channel.send(embed=embed)
+        except Exception as e:
+            self.logger.error("Failed to admin log message")
+            self.logger.error(e)
+
 
         if not (msg or embed):
             await channel.send("`bot_log()` was called without any arguments!")
 
 
-async def setup(bot: commands.Bot, logger: Logger, guild_id: str):
-    await bot.add_cog(admin_logging(bot, logger, guild_id))
+async def setup(bot: commands.Bot, logger: Logger, guild_id: int, channel_id: int):
+    await bot.add_cog(admin_logging(bot, logger, guild_id, channel_id))
